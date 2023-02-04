f5bfb08cfa31814c86823d177824660bfc208f86
Mohamed Alhaji Barrie <mbarrie981@gmail.com>
2023-02-04 17:53:39 +0000

    Mohamed Alhaji Barrie Edited the contact File

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,15 +8,20 @@ const Contact = () => {
           Contact Me
         </h2>
         <p>
+          Hello I'm Mohamed Alhaji Barrie. Student of the University of Management and Technology
+          we are currently learning on how to clone an existing Portfolio from the remote 
+          repository to out local repository. Thank you
+        </P>
+        <p>
           I am currently open for a fulltime Frontend Developer role. If you
           want to discuss about that feel free to email me or call me.
         </p>
 
         <p className="py-2">
-          <span className="font-bold">Email:</span> coderamrin@gmail.com
+          <span className="font-bold">Email:</span> mbarrie981@gmail.com
         </p>
         <p className="py-2">
-          <span className="font-bold">Phone:</span> +88 01624-890723
+          <span className="font-bold">Phone:</span> +23277-969-774
         </p>
       </div>
     </section>
